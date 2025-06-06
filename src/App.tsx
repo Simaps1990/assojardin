9afbacb8636f84e1a4d9709a25aeba90a3e0da94
Thomas <boyer_thomas@hotmail.fr>
2025-06-06 02:24:48 +0200

    maj auto

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ import ProtectedRoute from './ProtectedRoute';
 const PublicLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <>
     <Header />
-    <main className="pt-20">{children}</main> {/* ✅ Ajout de pt-20 pour compenser le header fixe */}
+    <main className="pt-0">{children}</main> {/* ✅ Ajout de pt-20 pour compenser le header fixe */}
     <Footer />
   </>
 );
