416a11f5deb1a4c0ae4cc6cc62caef2db5176f19
Thomas <boyer_thomas@hotmail.fr>
2025-06-07 18:48:33 +0200

    maj auto

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -77,7 +77,7 @@ return (
 {/* Section Météo + Plantation */}
 <MeteoConseilsSection />
 
-<div className="flex justify-center my-4">
+<div className="flex justify-center my-3">
       <hr className="w-1/2 border-t border-neutral-300" />
     </div>
 
